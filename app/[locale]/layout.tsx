cb027c36142a81458a9c4ee6f37bc83be8115c67
Katiaryna Dounar <ekaterinaaleks9@gmail.com>
2025-09-08 08:01:29 +0300

    fix: fix language switching bug

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import type { ReactNode } from 'react';
 
-import { hasLocale } from 'next-intl';
+import { hasLocale, NextIntlClientProvider } from 'next-intl';
 import { notFound } from 'next/navigation';
 
 import { routing } from '@/i18n/routing';
@@ -28,5 +28,5 @@ export default async function RootLayout({
   if (!hasLocale(routing.locales, locale)) {
     notFound();
   }
-  return <>{children}</>;
+  return <NextIntlClientProvider locale={locale}>{children}</NextIntlClientProvider>;
 }
